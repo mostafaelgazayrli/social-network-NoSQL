340e310d2557dafc71bc0e1d136c0a46ef8e437e
moustafaelgazayrli <elganes25@gmail.com>
2024-10-20 20:55:31 -0400

    edit

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -76,31 +76,35 @@ const thoughtController = {
       .catch((err) => res.json(err));
   },
 
-  // delete Thought
-  deleteThought({ params }, res) {
-    Thought.findOneAndDelete({ _id: params.id })
-      .then((dbThoughtData) => {
-        if (!dbThoughtData) {
-          return res.status(404).json({ message: "No thought with this id!" });
-        }
+// delete Thought
+deleteThought({ params }, res) {
+  Thought.findOneAndDelete({ _id: params.id })
+    .then((dbThoughtData) => {
+      if (!dbThoughtData) {
+        return res.status(404).json({ message: "No thought with this id!" });
+      }
 
-        // remove thought id from user's `thoughts` field
-        return User.findOneAndUpdate(
-          { thoughts: params.id },
-          { $pull: { thoughts: params.id } }, //$pull removes from an existing values that match a specified condition.
-          { new: true }
-        );
-      })
-      .then((dbUserData) => {
-        if (!dbUserData) {
-          return res
-            .status(404)
-            .json({ message: "Thought created but no user with this id!" });
-        }
-        res.json({ message: "Thought successfully deleted!" });
-      })
-      .catch((err) => res.json(err));
-  },
+      // Remove thought ID from the user's `thoughts` field
+      return User.findOneAndUpdate(
+        { thoughts: params.id },
+        { $pull: { thoughts: params.id } }, // Use $pull to remove the thought ID from the user's array
+        { new: true }
+      );
+    })
+    .then((dbUserData) => {
+      if (!dbUserData) {
+        return res
+          .status(404)
+          .json({ message: "No user found associated with this thought!" });
+      }
+      res.json({ message: "Thought successfully deleted!" });
+    })
+    .catch((err) => {
+      console.error(err);  // Log the error for debugging purposes
+      res.status(500).json({ message: "An error occurred", error: err.message });
+    });
+},
+
 
   // add reaction
   addReaction({ params, body }, res) {
